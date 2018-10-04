2eb922324312f4b81a6b6522a55d4a6c481604ef
Eduardo Aros <e.arosb@gmail.com>
2018-10-04 20:36:37 -0300

    RecyclerView parte dos

--- app/src/main/java/com/ufro/icc714/MainActivity.java
+++ app/src/main/java/com/ufro/icc714/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+// TODO (8) Implementa RepositoryAdapter.ListItemClickListener en la clase MainActivity
 public class MainActivity extends AppCompatActivity {
 
     private static final int NUM_LIST_ITEMS = 100;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private RepositoryAdapter mAdapter;
     private RecyclerView mNumbersList;
+
+    // TODO (9) Crea una variable de tipo Toast llamada mToast para guardar el Toast actual
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         // contenido no cambian el tamaño del diseño hijo en el RecyclerView
         mNumbersList.setHasFixedSize(true);
 
+        // TODO (13) Pasa this como segundo parámetro al constructor de RepositoryAdapter
         /*
          * RepositoryAdapter es el responsable de mostrar cada elemento en la lista
          */
@@ -41,4 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         mNumbersList.setAdapter(mAdapter);
     }
+
+    // TODO (10) Sobrescribe el método onListItemClick de ListItemClickListener
+    // TODO (11) Al principio del método cancela el toast si es distinto de nulo
+    // TODO (12) Muestra un toast cuando el item es cliqueado mostrado el número que fue cliqueado
 }
--- app/src/main/java/com/ufro/icc714/RepositoryAdapter.java
+++ app/src/main/java/com/ufro/icc714/RepositoryAdapter.java
@@ -6,26 +6,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder> {
 
-    // TODO (1) Crea un layout en res/layout/ llamado repository_list_item.xml
-
-    // haz los pasos 2 al 11 en repository_list_item.xml
-    // TODO (2) Haz el layout raíz un FrameLayout
-    // TODO (3) El ancho match_parent y el alto wrap_content
-    // TODO (4) Asigna un padding de 16dp
-    // TODO (5) Crea un TextView como el único hijo de FrameLayout
-    // TODO (6) El TextView debe tener un ID "@+id/tv_item_number"
-    // TODO (7) El ancho y alto deben ser wrap_content
-    // TODO (8) Alinea el TextView al inicio del padre
-    // TODO (9) Centra el TexView verticalmente en el layout
-    // TODO (10) Cambia el font family a monospace
-    // TODO (11) Asigna un tamaño de texto a 42sp
-
     private static final String TAG = RepositoryAdapter.class.getSimpleName();
 
+    // TODO (3) Crea un objeto final privado de tipo ListItemClickListener llamado mOnClickListener
+
     private int mNumberItems;
+
+    // TODO (1) Crea una interfaz anidada llamada ListItemClickListener
+    // TODO (2) Dentro de la interfaz define un método llamado onListItemClick que reciba un entero como parámetro
+
+    // TODO (4) Añade un objeto ListItemClickListener como parámetro al constructor y guardalo en mOnClickListener
 
     public RepositoryAdapter(int mNumberItems) {
         this.mNumberItems = mNumberItems;
@@ -34,7 +28,7 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
     @Override
     public RepositoryViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         Context context = viewGroup.getContext();
-        int layoutIdForListItem = R.layout.number_list_item;
+        int layoutIdForListItem = R.layout.repository_list_item;
         LayoutInflater inflater = LayoutInflater.from(context);
         boolean shouldAttachToParentImmediately = false;
 
@@ -55,14 +49,32 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
         return mNumberItems;
     }
 
-    // TODO (12) Crea una clase llamada RepositoryViewHolder que herede de RecyclerView.ViewHolder
+    // TODO (5) Implementa la interfaz OnClickListener en la clase NumberViewHolder class
+    /**
+     *
+     */
+    public class RepositoryViewHolder extends RecyclerView.ViewHolder {
 
-    // TODO (13) En RepositoryViewHolder, Crear una variable de tipo TextView llamado listItemNumberView
+        TextView listItemNumberView;
 
-    // TODO (14) Crea un constructor para RepositoryViewHolder que acepte un parámetro de tipo View llamado itemView
-    // TODO (15) En el constructor llama a super(itemView) y encuentra a listItemNumberView por su ID usando itemView (itemView.find...)
+        /**
+         *
+         * @param itemView
+         */
+        public RepositoryViewHolder(View itemView) {
+            super(itemView);
+            listItemNumberView = itemView.findViewById(R.id.tv_item_number);
+            // TODO (7) LLama a setOnClickListener en itemView (usa 'this' como OnClickListener)
+        }
 
-    // TODO (16) En la clase RepositoryViewHolder, crea un método void llamado bind que acepte un parámetro de tipo entero llamado listIndex
-    // TODO (17) En bind, asigna el texto de listItemNumberView igual a listIndex
-    // TODO (18) Ten ciudado al asignar un entero usando setText, debes usar la representación en texto del valor de listIndex
+        /**
+         *
+         * @param listIndex
+         */
+        public void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
+
+        // TODO (6) Sobreescribe el método onClick, pasando la posición del item cliqueado (getAdapterPosition()) a mOnClickListener a traves del método onListItemClick
+    }
 }
